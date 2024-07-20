abc1394911446cf4161fd8925ff35de23887925a
AradiaG <reparacionesteknoles@gmail.com>
2024-07-20 17:07:20 -0600

    Update AradiaG.py

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AradiaG.py
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/AradiaG.py
@@ -1,1 +1,28 @@
+#Comentario en una linea
+# https://www.python.org/
+"""
+Esto es un comentario
+de varias lineas en my py.
+Dependerá de cada lenguaje.
+"""
+
+mi_variable = "Mi variable" #Esto es un dato que puede terminar mutando
+mi_varibale = "Nuevo valor de mi variable" #Le acabo de modificar el valor
+
+"""En Phyton no existen las constantes; si quisiera una "Constante" en Py, le pondría en Mayúsculas, esto funciona
+solo como una referencia para el código, pero se puede modificar porque no deja de ser una variable"""
+
+MY_CONSTANTE = "No deja de ser una variable"
+
+#Datos primitivos:
+
+my_int = 1
+my_float = 1.1
+my_char = Hola
+my_bool = True
+
+#Imprimir en consola: "¡Hola, [y el nombre de tu lenguaje]!"
+
+print("¡Hola, Python!")
+      
 
