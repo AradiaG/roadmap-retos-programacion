acf6fa3b18c5e7ff6474f98483524c73241394e2
AradiaG <reparacionesteknoles@gmail.com>
2024-07-27 02:28:40 +0000

    26/07

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/AradiaG.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/AradiaG.py
@@ -42,3 +42,52 @@ default_arg_greet("Sí tiene")
 def return_args_greet(greet, name):
     return f"Hola, {greet},{name}!"
 print(return_args_greet("Hi", "Brais"))
+
+#Con retorno de varios valores
+
+def multiple_return_greet():
+    return "Hola", "python"
+
+greet, name = multiple_return_greet()
+print(greet)
+print(name)
+
+#Con un número variable de argumentos
+
+def variable_arg_greet(*names):
+    for name in names:
+        print(f"¡Hola, {name}!")
+
+variable_arg_greet("Python", "Brais", "Rosy", "Comunidad")
+
+# Con un número variable de argumentos con palabra clave
+
+def variable_key_arg_greet(**names):
+    for key, value in names.items():
+        print(f"¡{value} ({key})!")
+
+variable_key_arg_greet(
+language="Python",
+name= "Brais",
+alias="Rosy",
+edad= 32
+)
+
+"""
+Funciones dentro de funciones
+"""
+
+def outer_function():
+    def inner_function():
+        print("Función interna: Hola, Python")
+    inner_function()
+
+outer_function()
+
+"""
+Funciones de lenguaje (built-in)
+"""
+
+print()
+
+
