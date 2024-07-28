32c0d3df7ed7ca16209966d84cff9e24b60cff33
AradiaG <reparacionesteknoles@gmail.com>
2024-07-28 02:08:45 +0000

    lesson 02 v.final

--- Roadmap/02 - FUNCIONES Y ALCANCE/python/AradiaG.py
+++ Roadmap/02 - FUNCIONES Y ALCANCE/python/AradiaG.py
@@ -88,6 +88,42 @@ outer_function()
 Funciones de lenguaje (built-in)
 """
 
-print()
+print(len("Rosy"))
+print(type(36))
+
+print("Rosy".upper())
+
+"""
+Variables locales y globales
+"""
+
+global_var = "Pythones"
+
+print(global_var)
+
+def hello_python():
+    print(f"Hola {global_var}!")
+
+"""
+Extra
+"""
+
+def print_numbers(text_1, text_2) -> int:
+    count = 0
+    for number in range(1, 101):
+        if number % 3 == 0 and number % 5 == 0:
+            print(f"{text_1}{text_2}")
+        elif number % 3 == 0:
+            print(text_1)
+        elif number % 5 == 0:
+            print(text_2) 
+        else:
+            print(number)
+            count += 1
+    return count
+
+print(print_numbers("Rosy","Tita"))
+
+
 
 
